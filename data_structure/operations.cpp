4cc1ab593c9fd1c5e47de5347eac9ea022881cb6
irfanshadikrishad <irfanshadikofficial@gmail.com>
2023-02-03 00:39:18 +0600

    operations in ds: traversal

--- data_structure/operations.cpp
+++ data_structure/operations.cpp
@@ -2,14 +2,20 @@
 using namespace std;
 
 int main(){
-    int arr[] = {1, 2, 3, 4, 5, 6};
-
-    // Traversal : Traversing in Data Structure means systematically visiting every element of it. 
-    // Traversing is a process in which each element of a data structure is accessed. 
-    // Accessing an element of data structure means visiting every element at least once.
-    cout << "Traversal:" << endl;
-    for(int j=0; j<=(sizeof(arr)/sizeof(arr[0]))-1; j++){
-        cout << "arr[" << j << "] : " << arr[j] << endl;
+    int arr[] = {1, 2, 3, 4, 5, 6}, opera;
+    while(true){
+        cout << "====================\nSelect an operation: " << endl << "\n 1. Insert \n 2. Delete \n 3. Search \n 4. Sort \n 5. Merge \n 6. Traversal \n 7. Exit \n====================" << endl;
+        cout << "Insert your operation: ";
+        cin >> opera;
+        // Exit
+        if(opera == 7){
+            break;
+        // Traversal
+        }else if(opera == 6){
+            cout << "Traversal:" << endl;
+            for(int j=0; j<=(sizeof(arr)/sizeof(arr[0]))-1; j++){
+                cout << "arr[" << j << "] : " << arr[j] << endl;
+            }
+        }
     }
-    
 }
