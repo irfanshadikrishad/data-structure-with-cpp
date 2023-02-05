80bf6641e1ea929c9ecaef0399dc0817ebdcf3a5
irfanshadikrishad <irfanshadikofficial@gmail.com>
2023-02-05 19:31:36 +0600

    b01

---
+++ cpp_problem_solving/b01_print_number.cpp
@@ -0,0 +1,10 @@
+// C++ Program to Print Number (Entered by the User)
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cout << "Enter a number: ";
+    cin >> x;
+    cout << "\nYou entered " << x;
+}
