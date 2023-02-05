34b13e562b3511b9bfefa38d53016a3922dee66f
irfanshadikrishad <irfanshadikofficial@gmail.com>
2023-02-05 23:12:25 +0600

    codezclub_07

---
+++ cpp_problem_solving/codezclub_07_char2ASCII.cpp
@@ -0,0 +1,13 @@
+// C++ Program to Find ASCII Value of a Character
+#include <iostream>
+using namespace std;
+
+int main(){
+    char c;
+    cout << "..." << endl;
+    cout << "Enter a charecter: ";
+    cin >> c;
+    cout << "The ASCII value of " << c << " is: " << int(c) << endl;
+    cout << "..." << endl;
+
+}
