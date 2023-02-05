06fe95ee52ba595f975171d868889c7f6026e2c0
irfanshadikrishad <irfanshadikofficial@gmail.com>
2023-02-05 23:06:53 +0600

    codezclub_06

---
+++ cpp_problem_solving/codezclub_06_multiply2numbers.cpp
@@ -0,0 +1,16 @@
+// C++ Program to Multiply two Numbers
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x, y;
+    cout << "..." << endl;
+    cout << "n1     :";
+    cin >> x;
+    cout << "n2     :";
+    cin >> y;
+    cout << "..." << endl;
+    cout << "Multiply of " << x << "and " << y << " is :" << x*y << endl;
+    cout << "..." << endl;
+
+}
