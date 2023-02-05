fe70d4923548010eecc859692eed807447b71a03
irfanshadikrishad <irfanshadikofficial@gmail.com>
2023-02-05 22:59:01 +0600

    codezclub_05

---
+++ cpp_problem_solving/codezclub_05_sizeof.cpp
@@ -0,0 +1,17 @@
+// C++ Program to Find Size of int, float, double and char
+#include <iostream>
+using namespace std;
+
+int main(){
+    int m = 1;
+    char n = n;
+    double o = 1.65;
+    string p = "Hello";
+
+    cout << "..." << endl;
+    cout << "Size of int is     : " << sizeof(m) << endl;
+    cout << "Size of char is    : " << sizeof(n) << endl;
+    cout << "Size of double is  : " << sizeof(o) << endl;
+    cout << "Size of string is  : " << sizeof(p) << endl;
+    cout << "..." << endl;
+}
