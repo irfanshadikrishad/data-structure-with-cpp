5c2d811b3a84d0482d390fd7c15f030c7cca34c5
irfanshadikrishad <irfanshadikofficial@gmail.com>
2023-02-05 22:39:53 +0600

    codezclub_03

---
+++ cpp_problem_solving/codezclub_03_Quotient_Reminder.cpp
@@ -0,0 +1,16 @@
+// C++ Program to Find Quotient and Remainder
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x, y;
+    cout << "..." << endl;
+    cout << "Divider: ";
+    cin >> x;
+    cout << "Divisor: ";
+    cin >> y;
+    cout << "..." << endl;
+    cout << "Quotient: " << x / y << endl;
+    cout << "Reminder: " << x % y << endl;
+    cout << "..." << endl;
+}
