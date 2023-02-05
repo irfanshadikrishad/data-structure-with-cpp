785326409df937172a3211f611de385309fc5d2b
irfanshadikrishad <irfanshadikofficial@gmail.com>
2023-02-05 22:28:10 +0600

    codezclub_02

---
+++ cpp_problem_solving/codezclub_02_addition.cpp
@@ -0,0 +1,13 @@
+// C++ Program to Addition of Two Numbers
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x, y;
+    cout << "\nNumber 1:";
+    cin >> x;
+    cout << "\nNumber 2:";
+    cin >> y;
+
+    cout << endl << "Additon of " << x << " and " << y << " is :" << x+y << "\n ";
+}
